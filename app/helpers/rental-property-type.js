f6c4a881d05d9af38d1c99d4db80c65ac4b4d339
Kidon Chu <kidonchu@gmail.com>
2017-03-02 23:58:23 -0600

    Add comments to rental-property-type helper

--- app/helpers/rental-property-type.js
+++ app/helpers/rental-property-type.js
@@ -1,11 +1,34 @@
 import Ember from 'ember';
 
+/**
+ * @class RentalPropertyType
+ * @namespace Helper
+ */
+
+/**
+ * List of property types that are considered "Community"
+ *
+ * @property communityPropertyTypes
+ * @final
+ * @type String[]
+ */
 const communityPropertyTypes = [
   'Condo',
   'Townhouse',
   'Apartment'
 ];
 
+/**
+ * Returns rental's property type
+ *
+ * If `type` is one of
+ * {{#crossLink "Helper.RentalPropertyType/communityPropertyTypes:property"}}
+ * {{/crossLink}}, it returns "Community". Otherwise, returns "Standalone".
+ *
+ * @method rentalPropertyType
+ * @param {String} [type] Type of rental property
+ * @return {String} Type of rental property
+ */
 export function rentalPropertyType([type]) {
   if (communityPropertyTypes.contains(type)) {
     return 'Community';
