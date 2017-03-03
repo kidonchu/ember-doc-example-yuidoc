9b3faa1329ad824cee419e2cd7152f0af2d80617
Kidon Chu <kidonchu@gmail.com>
2017-03-03 00:07:42 -0600

    Comment maps service

--- app/services/maps.js
+++ app/services/maps.js
@@ -1,31 +1,52 @@
 import Ember from 'ember';
 import MapUtil from '../utils/google-maps';
 
+/**
+ * @class Maps
+ * @namespace Service
+ * @uses Util.GoogleMaps
+ */
 export default Ember.Service.extend({
 
-  init() {
-    if (!this.get('cachedMaps')) {
-      this.set('cachedMaps', Ember.Object.create());
-    }
-    if (!this.get('mapUtil')) {
-      this.set('mapUtil', MapUtil.create());
-    }
-  },
+	/**
+	 * @method init
+	 */
+	init() {
+		if (!this.get('cachedMaps')) {
+			this.set('cachedMaps', Ember.Object.create());
+		}
+		if (!this.get('mapUtil')) {
+			this.set('mapUtil', MapUtil.create());
+		}
+	},
 
-  getMapElement(location) {
-    let camelizedLocation = location.camelize();
-    let element = this.get(`cachedMaps.${camelizedLocation}`);
-    if (!element) {
-      element = this.createMapElement();
-      this.get('mapUtil').createMap(element, location);
-      this.set(`cachedMaps.${camelizedLocation}`, element);
-    }
-    return element;
-  },
+	/**
+	 * Fetches a map element
+	 *
+	 * @method getMapElement
+	 * @param {String} location
+	 * @return {Element}
+	 */
+	getMapElement(location) {
+		let camelizedLocation = location.camelize();
+		let element = this.get(`cachedMaps.${camelizedLocation}`);
+		if (!element) {
+			element = this.createMapElement();
+			this.get('mapUtil').createMap(element, location);
+			this.set(`cachedMaps.${camelizedLocation}`, element);
+		}
+		return element;
+	},
 
-  createMapElement() {
-    let element = document.createElement('div');
-    element.className = 'map';
-    return element;
-  }
+	/**
+	 * Creates a div element with *map* as class name
+	 *
+	 * @method createMapElement
+	 * @return {Element}
+	 */
+	createMapElement() {
+		let element = document.createElement('div');
+		element.className = 'map';
+		return element;
+	}
 });
