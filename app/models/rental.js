c883372826884a6b7d9abc2fc31c7b7eb49c0566
Kidon Chu <kidonchu@gmail.com>
2017-03-02 23:58:05 -0600

    Add comments to rental model

--- app/models/rental.js
+++ app/models/rental.js
@@ -1,11 +1,43 @@
 import DS from 'ember-data';
 
+/**
+ * @class Rental
+ * @namespace Model
+ */
 export default DS.Model.extend({
-  title: DS.attr(),
-  owner: DS.attr(),
-  city: DS.attr(),
-  type: DS.attr(),
-  image: DS.attr(),
-  bedrooms: DS.attr(),
-  description: DS.attr()
+	/**
+	 * @attribute title
+	 * @type String
+	 */
+	title: DS.attr(),
+	/**
+	* @attribute owner
+	* @type String
+	*/
+	owner: DS.attr(),
+	/**
+	* @attribute city
+	* @type String
+	*/
+	city: DS.attr(),
+	/**
+	* @attribute type
+	* @type String
+	*/
+	type: DS.attr(),
+	/**
+	* @attribute image
+	* @type String
+	*/
+	image: DS.attr(),
+	/**
+	* @attribute bedrooms
+	* @type Integer
+	*/
+	bedrooms: DS.attr(),
+	/**
+	* @attribute description
+	* @type String
+	*/
+	description: DS.attr()
 });
