2b1344f4288bd7a849c47a52cfefb827a460467d
Kidon Chu <kidonchu@gmail.com>
2017-03-03 09:11:39 -0600

    Docs

---
+++ api.js
@@ -0,0 +1,27 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Adapter.Application",
+        "Component.ListFilter",
+        "Component.ListingTable",
+        "Component.ListingTable.ListingRow",
+        "Component.LocationMap",
+        "Component.RentalListing",
+        "Controller.Rentals",
+        "Controller.Rentals.Index",
+        "Helper.RentalPropertyType",
+        "Model.Rental",
+        "Route.About",
+        "Route.Contact",
+        "Route.Index",
+        "Route.Rentals",
+        "Route.Rentals.Index",
+        "Route.Rentals.Show",
+        "Service.Maps",
+        "Util.GoogleMaps"
+    ],
+    "modules": [],
+    "allModules": [],
+    "elements": []
+} };
+});
